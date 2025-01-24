0f23f3b2650e9ca8870b47e726088a8cd90d54aa
Rikiub <rikiub.dev@gmail.com>
2025-01-24 12:59:47 -0400

    Update

--- sway/dot-local/share/light-mode.d/gtk-theme.bak.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-gsettings set org.gnome.desktop.interface gtk-theme    ${GTK_THEME_DARK:-adw-gtk3}
-gsettings set org.gnome.desktop.interface icon-theme   $GTK_ICON_LIGHT
-gsettings set org.gnome.desktop.interface cursor-theme $GTK_CURSOR_LIGHT
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
