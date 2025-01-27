12aec1118834c1876d0dd71de0632dd81ec0725c
Rikiub <rikiub.dev@gmail.com>
2025-01-27 13:46:56 -0400

    Update

--- sway/dot-local/share/light-mode.d/gtk-theme.sh
+++ sway/dot-local/share/light-mode.d/gtk-theme.sh
@@ -3,4 +3,4 @@
 gsettings set org.gnome.desktop.interface gtk-theme    "adw-gtk3"
 gsettings set org.gnome.desktop.interface icon-theme   "Papirus-Light"
 gsettings set org.gnome.desktop.interface cursor-theme "Qogir"
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
